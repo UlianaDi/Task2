a29c053e00eb34e944d9a939c3948a282f398b3f
UlianaDi <99318121+UlianaDi@users.noreply.github.com>
2022-06-03 11:52:13 +0300

    Add files via upload

---
+++ Secondtask/src/com/company/First.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class First {
+    void dobutok() {
+        int n = 1;
+        for (int m = 1; m <= 5; m++) {
+            n = n * m;
+        }
+        System.out.println("Число добутку: "+ n);
+    }
+}
